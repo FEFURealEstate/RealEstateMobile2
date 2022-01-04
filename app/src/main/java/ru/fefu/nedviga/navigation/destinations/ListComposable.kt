8185aa18716d7bd80fee0298f6233db66c91123f
travka47 <travka.eal@students.dvfu.ru>
2022-01-04 15:03:30 +1000

    Addition tasks

--- app/src/main/java/ru/fefu/nedviga/navigation/destinations/ListComposable.kt
+++ app/src/main/java/ru/fefu/nedviga/navigation/destinations/ListComposable.kt
@@ -1,6 +1,7 @@
 package ru.fefu.nedviga.navigation.destinations
 
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
@@ -9,6 +10,7 @@ import ru.fefu.nedviga.ui.screens.list.ListScreen
 import ru.fefu.nedviga.ui.viewmodels.SharedViewModel
 import ru.fefu.nedviga.util.Constants.LIST_ARGUMENT_KEY
 import ru.fefu.nedviga.util.Constants.LIST_SCREEN
+import ru.fefu.nedviga.util.toAction
 
 @ExperimentalMaterialApi
 fun NavGraphBuilder.listComposable(
@@ -20,7 +22,13 @@ fun NavGraphBuilder.listComposable(
         arguments = listOf(navArgument(LIST_ARGUMENT_KEY) {
             type = NavType.StringType
         })
-    ) {
+    ) { navBackStackEntry ->
+        val action = navBackStackEntry.arguments?.getString(LIST_ARGUMENT_KEY).toAction()
+
+        LaunchedEffect(key1 = action) {
+            sharedViewModel.action.value = action
+        }
+
         ListScreen(navigateToTaskScreen = navigateToTaskScreen, sharedViewModel = sharedViewModel)
     }
 }
--- app/src/main/java/ru/fefu/nedviga/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/ru/fefu/nedviga/ui/viewmodels/SharedViewModel.kt
@@ -1,10 +1,12 @@
 package ru.fefu.nedviga.ui.viewmodels
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
@@ -12,6 +14,7 @@ import kotlinx.coroutines.launch
 import ru.fefu.nedviga.data.models.TaskType
 import ru.fefu.nedviga.data.models.ToDoTask
 import ru.fefu.nedviga.data.repositories.ToDoRepository
+import ru.fefu.nedviga.util.Action
 import ru.fefu.nedviga.util.RequestState
 import ru.fefu.nedviga.util.SearchAppBarState
 import javax.inject.Inject
@@ -20,10 +23,13 @@ import javax.inject.Inject
 class SharedViewModel @Inject constructor(
     private val repository: ToDoRepository
 ) : ViewModel() {
+    val action: MutableState<Action> = mutableStateOf(Action.NO_ACTION)
+
     val id: MutableState<Int> = mutableStateOf(0)
     val comment: MutableState<String> = mutableStateOf("")
     val description: MutableState<String> = mutableStateOf("")
     val taskType: MutableState<TaskType> = mutableStateOf(TaskType.MEETING)
+
     val searchAppBarState: MutableState<SearchAppBarState> =
         mutableStateOf(SearchAppBarState.CLOSED)
     val searchTextState: MutableState<String> = mutableStateOf("")
@@ -52,6 +58,42 @@ class SharedViewModel @Inject constructor(
                 _selectedTask.value = task
             }
         }
+    }
+
+    private fun addTask() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val toDoTask = ToDoTask(
+                comment = comment.value,
+                duration = description.value.toInt(),
+                type = taskType.value
+            )
+            repository.addTask(toDoTask = toDoTask)
+        }
+    }
+
+    fun handleDatabaseActions(action: Action) {
+        Log.d("SharedViewModel", action.name)
+        when (action) {
+            Action.ADD -> {
+                addTask()
+            }
+            Action.UPDATE -> {
+
+            }
+            Action.DELETE -> {
+
+            }
+            Action.DELETE_ALL -> {
+
+            }
+            Action.UNDO -> {
+
+            }
+            else -> {
+
+            }
+        }
+        this.action.value = Action.NO_ACTION
     }
 
     fun updateTaskFields(selectedTask: ToDoTask?) {
