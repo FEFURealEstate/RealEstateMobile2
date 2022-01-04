65147207e814eec69e46222d75ae2566720bdcf9
travka47 <travka.eal@students.dvfu.ru>
2022-01-04 23:11:11 +1000

    Another fixes

--- app/src/main/java/ru/fefu/nedviga/ui/theme/Color.kt
+++ app/src/main/java/ru/fefu/nedviga/ui/theme/Color.kt
@@ -9,14 +9,14 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-val MeetingTypeColor = Color(0xFFFF4A6D)
-val ShowingTypeColor = Color(0xFFFFF6A5)
-val CallingTypeColor = Color(0xFF00D9BB)
-val NoneTypeColor = Color(0xFFCFD8DC)
-
 val LightGray = Color(0xFFFCFCFC)
 val MediumGray = Color(0xFF9C9C9C)
 val DarkGray = Color(0xFF141414)
+
+val MeetingTypeColor = Color(0xFFFF4A6D)
+val ShowingTypeColor = Color(0xFFFFF6A5)
+val CallingTypeColor = Color(0xFF00D9BB)
+val NoneTypeColor = MediumGray
 
 val Colors.topAppBarContentColor: Color
 @Composable
